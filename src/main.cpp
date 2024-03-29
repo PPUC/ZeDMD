ccb721335ea2bf4ba8b8b00375ca2e987f26ddb0
Markus Kalkbrenner <git@kalki.de>
2024-03-29 10:39:16 +0100

    added 128x64 support

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #define PANEL_WIDTH 128  // Width: number of LEDs for 1 panel.
 #define PANEL_HEIGHT 64  // Height: number of LEDs.
 #define PANELS_NUMBER 2  // Number of horizontally chained panels.
+#elif ZEDMD_HD_HALF
+#define PANEL_WIDTH 128  // Width: number of LEDs for 1 panel.
+#define PANEL_HEIGHT 64  // Height: number of LEDs.
+#define PANELS_NUMBER 1  // Number of horizontally chained panels.
 #endif
 #ifndef PANEL_WIDTH
 #define PANEL_WIDTH 64   // Width: number of LEDs for 1 panel.
@@ -93,7 +97,12 @@
 
 #define TOTAL_WIDTH (PANEL_WIDTH * PANELS_NUMBER)
 #define TOTAL_WIDTH_PLANE (TOTAL_WIDTH >> 3)
+#ifdef ZEDMD_HD_HALF
+#define TOTAL_HEIGHT (PANEL_HEIGHT / 2)
+#endif
+#ifndef TOTAL_HEIGHT
 #define TOTAL_HEIGHT PANEL_HEIGHT
+#endif
 #define TOTAL_BYTES (TOTAL_WIDTH * TOTAL_HEIGHT * 3)
 #define ZONE_WIDTH (TOTAL_WIDTH / 16)
 #define ZONE_HEIGHT (TOTAL_HEIGHT / 8)
