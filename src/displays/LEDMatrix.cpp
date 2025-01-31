a3396b0bbcaab322037a3efcca8a1835140ddd97
pastor <142336369+PastorL69@users.noreply.github.com>
2025-01-31 01:29:42 +0100

    lat blanking test

--- src/displays/LEDMatrix.cpp
+++ src/displays/LEDMatrix.cpp
@@ -25,7 +25,7 @@ LedMatrix::LedMatrix() {
   HUB75_I2S_CFG mxconfig(PANEL_WIDTH, PANEL_HEIGHT, PANELS_NUMBER, pins);
   // Without setting clkphase to false, HD panels seem to flicker.
   mxconfig.clkphase = false;
-  mxconfig.latch_blanking = 2;
+  mxconfig.latch_blanking = 4;
   mxconfig.min_refresh_rate = 30;
   mxconfig.driver = HUB75_I2S_CFG::shift_driver::SHIFTREG;
 
