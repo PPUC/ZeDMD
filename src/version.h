ee56446f0606fda54ab5bbb13c99753b740e24ef
Markus Kalkbrenner <git@kalki.de>
2025-11-10 14:34:41 +0100

    prepare version 6.0.0

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 // Version constants
-#define ZEDMD_VERSION_MAJOR 5
-#define ZEDMD_VERSION_MINOR 2
+#define ZEDMD_VERSION_MAJOR 6
+#define ZEDMD_VERSION_MINOR 0
 #define ZEDMD_VERSION_PATCH 0
 
 #endif  // VERSION_H
