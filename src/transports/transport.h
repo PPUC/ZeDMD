e84cb1428ccea5d73a3875d2e3a345a0cac4bc5f
Markus Kalkbrenner <git@kalki.de>
2025-12-14 12:52:17 +0100

    updated dmdreader

--- src/transports/transport.h
+++ src/transports/transport.h
@@ -7,7 +7,7 @@
 
 class Transport {
  public:
-  enum { USB = 0, WIFI_UDP = 1, WIFI_TCP = 2, SPI = 3, LOOPBACK = 5 };
+  enum { USB = 0, WIFI_UDP = 1, WIFI_TCP = 2, SPI = 3 };
 
   Transport() = default;
 
@@ -27,7 +27,7 @@ class Transport {
     return m_type == USB        ? "USB     "
            : m_type == WIFI_UDP ? "WiFi UDP"
            : m_type == WIFI_TCP ? "WiFi TCP"
-                                : "SPI";
+                                : "SPI     ";
   }
 
   virtual bool loadConfig() { return true; }
