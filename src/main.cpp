97d5c8b81337a5fb75a5094653682842fb66b6f4
Jan Vos <jjcvos@hotmail.nl>
2026-01-30 20:20:06 +0100

    remove no color file loaded warning..

--- src/main.cpp
+++ src/main.cpp
@@ -144,8 +144,6 @@ uint8_t panelMinRefreshRate = 60;
 bool core_0_initialized = false;
 bool core_1_initialized = false;
 uint8_t loopbackColor = (uint8_t)Color::DMD_ORANGE;
-uint8_t warningShown = 0;
-uint32_t spiStartMs = 0;
 
 const char *ColorString(uint8_t color) {
   switch ((Color)color) {
@@ -198,7 +196,6 @@ static void Scale2xLoopback(const uint8_t *src, uint8_t *dst, uint16_t srcWidth,
   }
 }
 
-constexpr uint32_t kDmdreaderNoDataTimeoutMs = 20000;
 static const char *kDmdreaderNoDataLines[] = {
     "The colorization module is not", "sending anything. Check if the",
     "serum.cROMc file is the right", "one for your game, the ROM",
@@ -2344,7 +2341,6 @@ void loop() {
       *dst++ = Expand5To8[rgb565 & 0x1f];
     }
     Render();
-    if (warningShown < 30) warningShown++;
   } else if (transport->isLoopback()) {
     uint8_t *buffer = dmdreader_loopback_render();
     if (buffer != nullptr) {
@@ -2359,14 +2355,7 @@ void loop() {
       }
       Render();
     }
-  } else if (warningShown < 30) {
-    if (spiStartMs == 0) {
-      spiStartMs = millis();
-    } else if ((millis() - spiStartMs) >= kDmdreaderNoDataTimeoutMs) {
-      DrawDmdreaderNoDataWarning();
-      warningShown = 30;
-    }
-  }
+  } 
   tight_loop_contents();
 
 #else
